7db30ab2d82fc27454d9b2a6a0253082ed808510
ElchananCohen <elchanancohen83@gmail.com>
2024-01-08 17:34:52 +0200

    fix

--- backend/main.py
+++ backend/main.py
@@ -1,65 +1,18 @@
-# from flask import Flask, request, jsonify, session
-# from flask_cors import CORS
-# import StepAndRool as sar
-
-# app = Flask(__name__)
-# CORS(app, origins="https://tic-tac-toe-front-4a0m.onrender.com")
-# # CORS(app, origins="http://localhost:5173")
-
-# # app.secret_key = 'your_secret_key'
-
-# @app.route('/api/data', methods=['POST'])
-# def post_data():
-#     data = request.get_json()
-#     squares = data.get('squares', [])
-#     vsAI = data.get('vsAI')
-
-#     nested_array = [squares[i:i+3] for i in range(0, 9, 3)]
-#     squares = nested_array
-
-#     result = sar.make_step(squares, vsAI)
-
-#     if sar.win(squares) is False:
-#         return jsonify({"squares":squares , "win": False})
-#     else:
-#         result = sar.win(squares)
-#         return jsonify({"squares":squares , "win": result})
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True, port=5001)
-
-
-
-import os
 from flask import Flask, request, jsonify, session
 from flask_cors import CORS
 import StepAndRool as sar
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import unpad
-import base64
-import json
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = Flask(__name__)
 CORS(app, origins="https://tic-tac-toe-front-4a0m.onrender.com")
+# CORS(app, origins="http://localhost:5173")
 
-secret_key = bytes(os.getenv('SECRET_KEY'), 'utf-8')
+# app.secret_key = 'your_secret_key'
 
 @app.route('/api/data', methods=['POST'])
 def post_data():
     data = request.get_json()
-    encrypted_data = data.get('encryptedData')
-
-    cipher = AES.new(secret_key, AES.MODE_CBC)
-    decrypted_data = unpad(cipher.decrypt(base64.b64decode(encrypted_data)), AES.block_size)
-
-    decrypted_json = json.loads(decrypted_data.decode('utf-8'))
-
-    squares = decrypted_json.get('squares', [])
-    vsAI = decrypted_json.get('vsAI')
+    squares = data.get('squares', [])
+    vsAI = data.get('vsAI')
 
     nested_array = [squares[i:i+3] for i in range(0, 9, 3)]
     squares = nested_array
@@ -75,3 +28,59 @@ def post_data():
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
+
+
+
+# import os
+# from flask import Flask, request, jsonify
+# from flask_cors import CORS
+# import StepAndRool as sar
+# from Crypto.Cipher import AES
+# from Crypto.Util.Padding import unpad
+# import base64
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# app = Flask(__name__)
+# CORS(app, origins="http://localhost:5173")
+
+# @app.route('/api/data', methods=['POST'])
+# def post_data():
+#     data = request.get_json()
+
+# def decrypt_data(encrypted_data, secret_key):
+#     # Convert the base64-encoded string to bytes
+#     encrypted_bytes = base64.b64decode(encrypted_data)
+
+#     # Create an AES cipher object with the provided secret key and mode
+#     cipher = AES.new(secret_key, AES.MODE_CBC)
+
+#     # Decrypt the data and unpad it using PKCS7 padding
+#     decrypted_data = unpad(cipher.decrypt(encrypted_bytes), AES.block_size)
+
+#     # Convert the bytes to a string and return
+#     return decrypted_data.decode('utf-8')
+    
+
+#     secret_key = bytes(os.getenv('SECRET_KEY'), 'utf-8')
+#     encrypted_data = data['data']
+#     print("decrypt_data", decrypt_data(encrypted_data, secret_key))
+
+
+#     squares =[1,2,3,4,5,6,7,7,8,] #data.get('squares', [])
+#     vsAI = 2 #data.get('vsAI')
+
+#     nested_array = [squares[i:i+3] for i in range(0, 9, 3)]
+#     squares = nested_array
+
+#     result = sar.make_step(squares, vsAI)
+#     if sar.win(squares) is False:
+#         return jsonify({"squares": squares, "win": False})
+#     else:
+#         result = sar.win(squares)
+#         return jsonify({"squares": squares, "win": result})
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5001)
--- frontend/src/componentes/CrossAndCircle.jsx
+++ frontend/src/componentes/CrossAndCircle.jsx
@@ -1,128 +1,6 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import '../App.css';
-
-// export default function Game() {
-//   const [xIsNext, setxIsNext] = useState(false);
-//   const [errors, setErrors] = useState(false);
-//   const [squares, setSquares] = useState(Array(9).fill("")); // Array to store values for each square
-//   const [vsAI, setVsAI] = useState(false);
-//   const [calculateWinner, setCalculateWinner] = useState(false);
-//   const [count, setCount] = useState(1);
-
-
-//   // const backgroundImage = import.meta.env.VITE_BACKGROUND_IMAGE || 'url(./public/background.jpg)';
-
-//   useEffect(() => {
-//     if (calculateWinner) {
-//       // Delay the alert to allow the UI to update first
-//       const timeoutId = setTimeout(() => {
-//         alert(`Win is: ${calculateWinner}`);
-//         location.reload();
-//       }, 100);
-
-//       // Cleanup the timeout to avoid memory leaks
-//       return () => clearTimeout(timeoutId);
-//     }
-//   }, [calculateWinner]);
-
-//   const handleSubmit = async (newSquares) => {
-//     const data = {
-//       squares: newSquares,
-//       vsAI: vsAI
-//     };
-
-//     try {
-//       const response = await axios.post("https://tic-tac-toe-server-m4ks.onrender.com/api/data", data)
-//       // const response = await axios.post("http://127.0.0.1:5001/api/data", data);
-
-//       setCalculateWinner(response.data.win)
-//       const squaresFlat = response.data.squares.flat()
-
-//       if (calculateWinner) {
-//         alert(`win is: ${win}`);
-//         location.reload();
-//       }
-
-//       setSquares(squaresFlat);
-
-//       console.log(squares);
-//     } catch (error) {
-//       if (error.response && error.response.data && error.response.data.errors) {
-//         setErrors(error.response.data.errors);
-//         console.log(errors);
-//       }
-//     }
-//   };
-
-//   const onClick = (index) => {
-
-//     if (vsAI) {
-//       // Check if the square is already filled or the game is over
-//       if (squares[index] || calculateWinner) {
-//         return;
-//       }
-
-//       setxIsNext(!xIsNext);
-
-//       // Clone the squares array to avoid mutating state directly
-//       const newSquares = [...squares];
-
-//       if (vsAI === 2) {
-//         // Update the value for the clicked square based on xIsNext
-//         newSquares[index] = "X";
-//         // newSquares[index] = !xIsNext ? "X" : "O";
-//       }
-
-
-//       if (vsAI === 1) {
-//         if (count % 2 == 0) {
-//           newSquares[index] = "O";
-//         } else {
-//           newSquares[index] = "X";
-//         }
-//       }
-
-//       setCount(count + 1)
-//       // Update the state with the new squares array
-//       setSquares(newSquares);
-
-//       handleSubmit(newSquares, vsAI);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       {!vsAI && (
-//         <div className="req_vs">
-//           <h1>אני רוצה לשחק עם</h1>
-//           <div className="button">
-//             <button className="choose" onClick={() => setVsAI(1)}>חבר</button>
-//             <button className="choose" onClick={() => setVsAI(2)}>מחשב</button>
-//           </div>
-//         </div>
-//       )}
-      
-//       <div className="board">
-//         {squares.map((value, index) => (
-//           <button
-//             key={index}
-//             className={`square squares`}
-//             onClick={() => onClick(index)}
-//           >
-//             {value}
-//           </button>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import '../App.css';
-import CryptoJS from 'crypto-js';
 
 export default function Game() {
   const [xIsNext, setxIsNext] = useState(false);
@@ -153,12 +31,9 @@ export default function Game() {
       squares: newSquares,
       vsAI: vsAI
     };
-    const secretKey = import.meta.env.VITE_SECRET_KEY
-    const encryptedData = CryptoJS.AES.encrypt(JSON.stringify(data), secretKey).toString();
-
 
     try {
-      const response = await axios.post("https://tic-tac-toe-server-m4ks.onrender.com/api/data", encryptedData)
+      const response = await axios.post("https://tic-tac-toe-server-m4ks.onrender.com/api/data", data)
       // const response = await axios.post("http://127.0.0.1:5001/api/data", data);
 
       setCalculateWinner(response.data.win)
@@ -242,3 +117,128 @@ export default function Game() {
     </div>
   );
 };
+
+
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import '../App.css';
+// import CryptoJS from 'crypto-js';
+
+// export default function Game() {
+//   const [xIsNext, setxIsNext] = useState(false);
+//   const [errors, setErrors] = useState(false);
+//   const [squares, setSquares] = useState(Array(9).fill("")); // Array to store values for each square
+//   const [vsAI, setVsAI] = useState(false);
+//   const [calculateWinner, setCalculateWinner] = useState(false);
+//   const [count, setCount] = useState(1);
+
+
+//   // const backgroundImage = import.meta.env.VITE_BACKGROUND_IMAGE || 'url(./public/background.jpg)';
+
+//   useEffect(() => {
+//     if (calculateWinner) {
+//       const timeoutId = setTimeout(() => {
+//         alert(`Win is: ${calculateWinner}`);
+//         location.reload();
+//       }, 100);
+
+//       return () => clearTimeout(timeoutId);
+//     }
+//   }, [calculateWinner]);
+
+//   const handleSubmit = async (newSquares) => {
+//     const data = {
+//       squares: newSquares,
+//       vsAI: vsAI
+//     };
+
+//     const encryptData = (data) => {
+//       const secretKey =  import.meta.env.VITE_SECRET_KEY
+//       const encryptedData = CryptoJS.AES.encrypt(JSON.stringify(data), secretKey).toString();
+//       return {
+//         data: encryptedData
+//       };
+//     };
+
+//     console.log(12345, encryptData(data));
+
+//     try {
+//       const response = await axios.post("http://127.0.0.1:5001/api/data", encryptData(data));
+//       setCalculateWinner(response.data.win);
+//       setSquares(response.data.squares.flat());
+//       if (calculateWinner) {
+//         alert(`win is: ${win}`);
+//         location.reload();
+//       }
+
+//       console.log(squares);
+//     } catch (error) {
+//       if (error.response && error.response.data && error.response.data.errors) {
+//         setErrors(error.response.data.errors);
+//         console.log(errors);
+//       }
+//     }
+//   };
+
+//   const onClick = (index) => {
+
+//     if (vsAI) {
+//       // Check if the square is already filled or the game is over
+//       if (squares[index] || calculateWinner) {
+//         return;
+//       }
+
+//       setxIsNext(!xIsNext);
+
+//       // Clone the squares array to avoid mutating state directly
+//       const newSquares = [...squares];
+
+//       if (vsAI === 2) {
+//         // Update the value for the clicked square based on xIsNext
+//         newSquares[index] = "X";
+//         // newSquares[index] = !xIsNext ? "X" : "O";
+//       }
+
+
+//       if (vsAI === 1) {
+//         if (count % 2 == 0) {
+//           newSquares[index] = "O";
+//         } else {
+//           newSquares[index] = "X";
+//         }
+//       }
+
+//       setCount(count + 1)
+//       // Update the state with the new squares array
+//       setSquares(newSquares);
+
+//       handleSubmit(newSquares, vsAI);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       {!vsAI && (
+//         <div className="req_vs">
+//           <h1>אני רוצה לשחק עם</h1>
+//           <div className="button">
+//             <button className="choose" onClick={() => setVsAI(1)}>חבר</button>
+//             <button className="choose" onClick={() => setVsAI(2)}>מחשב</button>
+//           </div>
+//         </div>
+//       )}
+      
+//       <div className="board">
+//         {squares.map((value, index) => (
+//           <button
+//             key={index}
+//             className={`square squares`}
+//             onClick={() => onClick(index)}
+//           >
+//             {value}
+//           </button>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
